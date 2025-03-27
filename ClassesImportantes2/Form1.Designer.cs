163e62c45677aacdfd51754818554c1192da81f0
livia.psousa <livia.psousa@TIT0DVB6ZW11-1>
2025-03-26 21:18:42 -0300

    Adicionar arquivos de projeto.

---
+++ ClassesImportantes2/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ClassesImportantes2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMessageBox = new System.Windows.Forms.Button();
+            this.labelresult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMessageBox
+            // 
+            this.btnMessageBox.Location = new System.Drawing.Point(25, 386);
+            this.btnMessageBox.Name = "btnMessageBox";
+            this.btnMessageBox.Size = new System.Drawing.Size(131, 52);
+            this.btnMessageBox.TabIndex = 0;
+            this.btnMessageBox.Text = "MessageBox";
+            this.btnMessageBox.UseVisualStyleBackColor = true;
+            // 
+            // labelresult
+            // 
+            this.labelresult.AutoSize = true;
+            this.labelresult.Location = new System.Drawing.Point(371, 127);
+            this.labelresult.Name = "labelresult";
+            this.labelresult.Size = new System.Drawing.Size(35, 13);
+            this.labelresult.TabIndex = 1;
+            this.labelresult.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelresult);
+            this.Controls.Add(this.btnMessageBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMessageBox;
+        private System.Windows.Forms.Label labelresult;
+    }
+}
+
---
+++ ClassesImportantes2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassesImportantes2
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // MessageBox.Show("Olá, sou uma janela de menssagem");
+            // MessageBox.Show("Olá, sou uma janela de menssagem", "Título da minha janela");
+            //MessageBox.Show("Mensagem Na Janela", "Título da Janela", MessageBoxButtons.YesNoCancel);
+            //MessageBox.Show("Mensagem Na Janela", "Título da Janela", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            //MessageBox.Show("Mensagem Na Janela");
+            DialogResult resultadoJanela = MessageBox.Show("Mensagem da Janela", "Título da Janela",
+                MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+
+            if (resultadoJanela == DialogResult.Yes)
+            {
+                labelresult.Text = "Você selecionou a opção SIM";
+
+            }
+            else if (resultadoJanela == DialogResult.No)
+            {
+                labelresult.Text = "Você seleciounou a opção NÃO";
+            }
+
+            else
+            {
+                // labeltex.Text= "";
+                // labeltex.Text= null;
+                labelresult.Text = string.Empty;
+            }
+
+            //labeltex.Text = resultadoJanela.ToString();
+
+
+
+        }
+    }
+    }
+
